0ea2363053cad1eec1e7cb894b7c37d90b9d1064
Git-bashi <71607175+Git-bashi@users.noreply.github.com>
2021-05-24 21:56:33 +0900

    javascript課題提出
    条件分岐・演算子

---
+++ 1-3/main.js
@@ -0,0 +1,20 @@
+let a = 10;
+let b = 15;
+let x = 10;
+let y = 0;
+//問１
+console.log(10 !== a);
+console.log(10 <= b && b < 20);
+//問２
+if (20 >= x && x>= 10) {
+    console.log("成功です");
+} else {
+    console.log("失敗です");
+}
+//問３
+if (y % 2 === 0) {
+    console.log("偶数です");
+}
+if (y % 2 !== 0) {
+    console.log("奇数です");
+}
